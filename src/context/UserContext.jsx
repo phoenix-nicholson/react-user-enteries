05faf3923f71d9fa7728d61f22bf8ba1c4cc0381
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-02-07 16:43:25 -0800

    created UserContext

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,20 @@
+import { useContext, createContext, useState } from 'react';
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState([]);
+
+  return <UserContext.Provider value={(user, setUser)}>{children}</UserContext.Provider>;
+};
+
+const useUser = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new Error('useUser must be defined with in a user context provider');
+  }
+  return context;
+};
+
+export { useUser, UserProvider };
