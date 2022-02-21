8797f1607a6e57c8aee580fc2e5ef12d39efcd21
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-02-21 14:33:46 -0800

    checked out auth branch

---
+++ src/App.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+import { UserProvider } from './context/UserContext';
+
+test('components have behavior', () => {
+  render(
+    <UserProvider>
+      <App />
+    </UserProvider>
+  );
+
+  const userName = screen.getByRole('textbox', { name: /name:/i });
+  const name = 'phoenix';
+  expect(name).toBe('phoenix');
+
+  const userEntry = screen.getByRole('textbox', { name: /entry:/i });
+  const entry = 'hi';
+  expect(entry).toBe('hi');
+
+  const submitBtn = screen.getByRole('button', { name: /submit entry/i });
+
+  userEvent.type(userName, name);
+  userEvent.type(userEntry, entry);
+  userEvent.click(submitBtn);
+
+  expect(screen.getByRole('heading', { name: /phoenix/i }));
+});
