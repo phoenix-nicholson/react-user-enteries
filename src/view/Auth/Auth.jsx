8f6e9f527d0bd2aa98ea76d667c0f5b2af2cf60a
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-02-22 11:49:26 -0800

    fixing handleLogin function, changing from email to name

--- src/view/Auth/Auth.jsx
+++ src/view/Auth/Auth.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 import { useUser } from '../../context/UserContext';
@@ -6,18 +7,33 @@ export default function Auth() {
   const { setUser } = useUser();
   const history = useHistory();
   const location = useLocation();
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
 
   const { from } = location.state || { from: { pathname: '/' } };
 
   const handleLogin = (e) => {
     e.preventDefault();
-    setUser('miklo');
-    history.replace(from.pathname);
+
+    if (
+      name === process.env.REACT_APP_AUTH_NAME &&
+      password === process.env.REACT_APP_AUTH_PASSWORD
+    ) {
+      setUser({ name: name, passsword: password });
+      setName('');
+      setPassword('');
+      history.replace(from.pathname);
+    } else {
+      throw new Error('Login was not successful');
+    }
   };
+
   return (
     <>
       <h1>Sign In</h1>
       <form onSubmit={handleLogin}>
+        <input type="name" name="name" id="name" />
+        <input type="password" name="password" id="password" />
         <button type="submit">sign in miklo!</button>
       </form>
     </>
