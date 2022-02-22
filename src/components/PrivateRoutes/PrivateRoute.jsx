15217e05199f9654e9f48c5dda74096cba3ff009
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-02-21 17:19:00 -0800

    got started on PrivateRoutes

---
+++ src/components/PrivateRoutes/PrivateRoute.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { useUser } from '../../context/UserContext';
+
+export default function PrivateRoute({ children, ...routeProps }) {
+  const { user } = useUser();
+  return (
+    <Route
+      {...routeProps}
+      render={({ location }) =>
+        user ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/',
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
